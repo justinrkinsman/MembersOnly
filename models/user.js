d9c2d1e6a33e536e504dec46fe58475c3e318cff
Justin <justinrkinsman@gmail.com>
2022-11-22 11:35:14 -0400

    Add enum property to user model

--- models/user.js
+++ models/user.js
@@ -6,8 +6,12 @@ const UserSchema = new Shcema({
     first_name: { type: String, required: true, minLength: 1, maxLength: 100 },
     last_name: { type: String, required: true, minLength: 1, maxLength: 100 },
     username: {type: String, required: true},
-    password: {type: String},
-    membership: {type: Boolean},
+    password: {type: String, required: true},
+    membership: {
+        type: String,
+        enum: ['non-member', 'member'],
+        default: 'non-member'},
+    admin: {type: Boolean},
 })
 
 // Virtual for user's full name
