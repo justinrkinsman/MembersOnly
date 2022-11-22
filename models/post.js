4794f29589ad8e863b37621ff56aa2eb365c9cb8
Justin <justinrkinsman@gmail.com>
2022-11-22 11:23:28 -0400

    Create model for posts

---
+++ models/post.js
@@ -0,0 +1,19 @@
+const { DateTime } = require('luxon')
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const PostSchema = new Shcema({
+    title: { type: String, required: true, minLength: 1, maxLength: 100 },
+    post_body: { type: String, required: true, minLength: 1, maxLength: 100 },
+    timestamp: { type: Date, required: true },
+    password: { type: String },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+})
+
+// Formatted timestamp
+PostSchema.virtual("formatted_timestamp").get(function () {
+    return DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATE_MED)
+})
+
+module.exports = mongoose.model("Post", PostSchema)
