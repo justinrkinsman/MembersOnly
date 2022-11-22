08fe5ba571c753eec3effc24159725b42cec9669
Justin <justinrkinsman@gmail.com>
2022-11-22 13:36:03 -0400

    Add sign up controller to catalog

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,2 +1,7 @@
+const { body, validationResult } = require("express-validator")
 
+const async = require('async')
 
+exports.index = function (req, res, next) {
+    res.render('sign-up', { title: 'Sign Up' });
+};
--- routes/catalog.js
+++ routes/catalog.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+// Require controller modules
+const signup = require('../controllers/signupController')
+
 /* Get sign up page*/
-router.get('/', function(req, res, next) {
-    res.render('sign-up', { title: 'Sign Up' });
-});
+router.get('/', signup.index)
   
 module.exports = router;
