8e998b528e8d01cf5ef5abd2c3f4f1d4c1178c05
Justin <justinrkinsman@gmail.com>
2022-11-22 15:25:23 -0400

    Create login page

--- routes/catalog.js
+++ routes/catalog.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 // Require controller modules
 const signup = require('../controllers/signupController')
+const login = require('../controllers/loginController')
 
 /// SIGN UP ROUTES ///
 
@@ -11,5 +12,14 @@ router.get('/signup_form', signup.index)
 
 // POST sign up page
 router.post('/signup_form', signup.create_user_post)
+
+
+/// LOGIN ROUTES ///
+
+// GET login page
+router.get('/login', login.index)
+
+// POST login page
+router.post('/login', login.login_user)
 
 module.exports = router;
