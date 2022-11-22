d4463bc480be0514f1cdf0e4b515fda66520c28f
Justin <justinrkinsman@gmail.com>
2022-11-22 11:09:01 -0400

    install packages and scripts

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
