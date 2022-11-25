9db2b9cca4065b1acfda350dc81ce446182cedde
Justin <justinrkinsman@gmail.com>
2022-11-25 10:41:43 -0400

    Fill out new post page

--- routes/catalog.js
+++ routes/catalog.js
@@ -5,6 +5,7 @@ const app = require("../app")
 const bcrypt = require('bcryptjs')
 
 const User = require('../models/user')
+const Post = require('../models/post')
 
 const async = require('async')
 
@@ -13,6 +14,10 @@ const async = require('async')
 // GET sign up page
 router.get('/signup_form', (req, res, next) => {
     res.render('signup_form.pug', { title: 'Sign Up' });
+})
+
+router.get('/new-post', (req, res, next) => {
+    res.render('new_post.pug')
 })
 
 module.exports = router;
