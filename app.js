24fdcc42e62f97212766435d025d2b67978445f8
Justin <justinrkinsman@gmail.com>
2022-12-05 10:39:56 -0400

    Add env variable to mongoose.connect

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use('/users', usersRouter);
 const mongoose = require("mongoose")
 const dev_db_url = process.env.MONGO_URL
 const mongoDB = dev_db_url
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoDB || MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on("error", console.error.bind(console, "MongoDB connection error:"))
 
