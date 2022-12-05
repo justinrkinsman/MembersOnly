3746c6e5c28be1c8134440575ed66f3b430e4844
Justin <justinrkinsman@gmail.com>
2022-12-05 10:01:03 -0400

    Change express-session to cookie-session

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const session = require("express-session")
+const session = require("cookie-session")
 const passport = require("passport")
 const LocalStrategy = require('passport-local').Strategy
 const cookieParser = require('cookie-parser');
