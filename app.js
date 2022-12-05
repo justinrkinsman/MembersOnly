a528b786fe312c2d8312705c854f348f3d1c0ddc
Justin <justinrkinsman@gmail.com>
2022-12-05 10:25:07 -0400

    Push for railway

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ dotenv.config()
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const session = require("express-session")
+const session = require("cookie-session")
 const passport = require("passport")
 const LocalStrategy = require('passport-local').Strategy
 const cookieParser = require('cookie-parser');
